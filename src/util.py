dfae278a162a105da9ac2df0160613ceb9cfa7a3
Eduardo Bezerra <ebezerra@cefet-rj.br>
2023-03-28 11:24:10 -0300

    Implement function util.utc_to_local

--- src/util.py
+++ src/util.py
@@ -1,3 +1,6 @@
+import pytz
+from datetime import datetime
+
 def is_posintstring(s):
     try:
         temp = int(s)
@@ -8,3 +11,50 @@ def is_posintstring(s):
     except ValueError:
         return False
 
+def format_time(input_str):
+    '''
+    This function first converts the input string to an integer using the int function. 
+    It then extracts the hours and minutes from the input integer using integer division and modulus operations. 
+    Finally, it formats the output string using f-strings to ensure that both hours and minutes are represented with two digits.
+    Usage examples:
+        print(format_time("100"))   # Output: "01:00"
+        print(format_time("1200"))  # Output: "12:00"
+        print(format_time("2300"))  # Output: "23:00"
+    '''
+    # Convert input string to integer
+    input_int = int(input_str)
+    
+    # Extract hours and minutes from the input integer
+    hours = input_int // 100
+    minutes = input_int % 100
+    
+    # Format the output string
+    output_str = f"{hours:02}:{minutes:02}"
+    
+    return output_str
+
+def utc_to_local(utc_string, local_tz, format_string):
+    '''
+    The function first converts the UTC string to a datetime object with timezone information 
+    using strptime() and replace(). Then it converts the datetime object to the local timezone 
+    using astimezone(). Finally, it formats the resulting datetime as a string in the same
+    format as the input string.
+
+    Here's an example usage of the function:
+    
+        utc_string = '1972-09-13 12:00:00'
+        local_tz = 'America/Sao_Paulo'
+        format_string = '%Y-%m-%d %H:%M'
+        local_string = utc_to_local(utc_string, local_tz, format_string)
+        print(local_string)
+
+    '''
+    # convert utc string to datetime object
+    utc_dt = datetime.strptime(utc_string, format_string).replace(tzinfo=pytz.UTC)
+    
+    # convert to local timezone
+    local_tz = pytz.timezone(local_tz)
+    local_dt = utc_dt.astimezone(local_tz)
+    
+    # format as string and return
+    return local_dt.strftime(format_string)
