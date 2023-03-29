7d310fba0a2a88704877c705b3c8f22462db6c65
Eduardo Bezerra <ebezerra@cefet-rj.br>
2023-03-29 11:16:58 -0300

    Add function transform_hour

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -7,6 +7,8 @@ import getopt
 import xarray as xr
 from utils.near_stations import prox
 from datetime import datetime, timedelta
+from metpy.calc import wind_components
+from metpy.units import units
 
     # if arquivo in cor_est:
     #     wind_speed = df['VelVento']
@@ -14,30 +16,47 @@ from datetime import datetime, timedelta
     # else:
     #     wind_speed = df['VEN_VEL']
     #     wind_direction = df['VEN_DIR']
+# def transform_wind(wind_speed, wind_direction):
+#     wv = wind_speed / 3.6
+#     wd_rad = wind_direction * np.pi / 180
+#     wind_x = wv * np.cos(wd_rad)
+#     wind_y = wv * np.sin(wd_rad)
+#     return wind_x, wind_y
+
 def transform_wind(wind_speed, wind_direction):
-    wv = wind_speed / 3.6
-    wd_rad = wind_direction * np.pi / 180
-    wind_x = wv * np.cos(wd_rad)
-    wind_y = wv * np.sin(wd_rad)
-    return wind_x, wind_y
-    
+    """
+    Calculate the U, V wind vector components from the speed and direction.
+    """
+    return wind_components(wind_speed * units('m/s'), wind_direction * units.deg)
+
     # if arquivo in cor_est:
     #     date_time = pd.to_datetime(df['Dia'] +' '+ df['Hora'], format='%Y-%m-%d%H:%M:%S', infer_datetime_format=True)
     # else:
     #     date_time = pd.to_datetime(df['DT_MEDICAO'] + ' '+ df['HR_MEDICAO'].apply(lambda x: '{0:0>4}'.format(x)).str.slice(0, 2) + ':' + df['HR_MEDICAO'].apply(lambda x: '{0:0>4}'.format(x)).str.slice(2, 4) + ':00', format='%Y-%m-%d%H:%M:%S', infer_datetime_format=True)
-def transform_time(df, date_time):
-    timestamp_s = date_time.map(pd.Timestamp.timestamp)
-    day = 24*60*60
-    year = (365.2425)*day
-    df['Day sin'] = np.sin(timestamp_s * (2 * np.pi / day))
-    df['Day cos'] = np.cos(timestamp_s * (2 * np.pi / day))
-    df['Year sin'] = np.sin(timestamp_s * (2 * np.pi / year))
-    df['Year cos'] = np.cos(timestamp_s * (2 * np.pi / year))
+# def transform_time(df, date_time):
+#     timestamp_s = date_time.map(pd.Timestamp.timestamp)
+#     day = 24*60*60
+#     year = (365.2425)*day
+#     df['Day sin'] = np.sin(timestamp_s * (2 * np.pi / day))
+#     df['Day cos'] = np.cos(timestamp_s * (2 * np.pi / day))
+#     df['Year sin'] = np.sin(timestamp_s * (2 * np.pi / year))
+#     df['Year cos'] = np.cos(timestamp_s * (2 * np.pi / year))
 
 def transform_hour(df):
-    df['hourfloat'] = df.hour + df.minute/60.0
-    df['hour_x'] = np.sin(2.*np.pi*df.hourfloat/24.)
-    df['hour_y'] = np.cos(2.*np.pi*df.hourfloat/24.)
+    """
+    Transforms a DataFrame's datetime index into two new columns representing the hour in sin and cosine form.
+
+    Args:
+    - df: A pandas DataFrame with a datetime index.
+
+    Returns:
+    - A pandas DataFrame with two new columns named 'hour_sin' and 'hour_cos' representing the hour in sin and cosine form.
+    """
+    dt = df.index
+    hourfloat = dt.hour + dt.minute/60.0
+    df['hour_sin'] = np.sin(2. * np.pi * hourfloat/24.)
+    df['hour_cos'] = np.cos(2. * np.pi * hourfloat/24.)
+    return df
 
 def main(argv):
     arg_file = ""
